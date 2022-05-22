d428701675d153c3134c646baa698bc79fc4c5b5
boraugur07 <105790014+boraugur07@users.noreply.github.com>
2022-05-22 16:46:56 +0300

    commit

--- src/App.java
+++ src/App.java
@@ -21,9 +21,37 @@ public class App {
         ArrayList<Facility> openedFacilities = new ArrayList<>();
         ArrayList<Point> unassignedPoints = new ArrayList<>();
         ArrayList<ArrayList<Point>> assignmentList = new ArrayList<>();
+        double[][] Minfacility = operator.createDistanceMatrix(5,2);
+        for(int i=0; i<distanceMatrix.length;i++) {
+        	for(int j=0; j<distanceMatrix[0].length;j++) {
+        		if(operator.findMinIndex(distanceMatrix[i][j])==facs_coordinates[i][j]) {
+        		Minfacility[i][j]=facs_coordinates[i][j];
+        		 for (int a=0; i<facs.length;i++){
+        		if(facs[a].getX()== Minfacility[i][0]&& facs[a].getY()== Minfacility[0][j]) { 
+        			openedFacilities.add(facs[i]);
+        		}
+        		 }
+        		}
+        	}
+        }
+        		 double[][] Minpoints= operator.createDistanceMatrix(20,2);
+         		 for (int a=0; i<points.length;i++){
+         			Minpoints[i][j]=points_coordinates[i][j];
+         		 if(points[a].getX()== Minpoints[i][0]&& points[a].getY()== Minpoints[0][j]) { 
+         			unassignedPoints.add(points[i]);
         
-    }
+         		}
+         		 
+         		 }
+                 for(int i=0; i<openedFacilities.size();i++) {
+                	 for(int j=0; j<unassignedPoints.size();j++) {
+                		 if(unassignedPoints.)
+                	 }
+                 }
+                	 
+                 }
+         		 
 }
 
-
+        		     		
 
